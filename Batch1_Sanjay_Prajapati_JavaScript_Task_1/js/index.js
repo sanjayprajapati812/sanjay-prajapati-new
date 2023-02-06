4540af81313ca772a611ddd4a3fc34ba8d43035f
sanjayprajapati812 <svprajapati812@gmail.com>
2023-02-06 23:51:27 +0530

    jnjnkjnj

--- Batch1_Sanjay_Prajapati_JavaScript_Task_1/js/index.js
+++ Batch1_Sanjay_Prajapati_JavaScript_Task_1/js/index.js
@@ -1,10 +1,28 @@
+const childDivElement = document.getElementById("childDivElement").innerHTML;
+// console.log(childDivElement);
+let count = 1;
 
-const nameInputDiv ="`"+ document.getElementById("nameInputDiv")+"`";
-const minusBtnDiv = document.getElementById("minusBtnDiv");
-
-function addElement(){
-    document.getElementById("divElement").innerHTML+=nameInputDiv;
-    document.getElementById("divElement").innerHTML+=minusBtnDiv;
-    
+function addElement() {
+  if (count < 10) {
+    const elCreated = document.createElement(`div`);
+    document.getElementById("divElement").appendChild(elCreated);
+    elCreated.setAttribute("id", count);
+    document.getElementById(count).innerHTML = childDivElement;
+    count++;
+  } else {
+    document.getElementById("limitShow").innerHTML = "limit over";
+  }
 }
 
+function removeElement(currEle) {
+  if (count <= 1) {
+    document.getElementById("minusBtn").disabled = true;
+    count = count - 1;
+  } else {
+    document.getElementById("minusBtn").disabled = false;
+    const findCurr = document.getElementById(currEle);
+    //   console.log(a.parentNode.parentNode.parentNode);
+    findCurr.parentNode.parentNode.parentNode.remove();
+    count--;
+  }
+}
