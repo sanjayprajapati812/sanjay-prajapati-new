cef140d22824b69da53ed5d574ce8e73d5e48efb
sanjayprajapati812 <svprajapati812@gmail.com>
2023-02-06 16:56:52 +0530

    js

---
+++ Batch1_Sanjay_Prajapati_JavaScript_Task_1/js/index.js
@@ -0,0 +1,10 @@
+
+const nameInputDiv = document.getElementById("nameInputDiv");
+const minusBtnDiv = document.getElementById("minusBtnDiv");
+
+function addElement(){
+    document.getElementById("divElement").innerHTML=nameInputDiv;
+    document.getElementById("divElement").innerHTML=minusBtnDiv;
+    
+}
+
