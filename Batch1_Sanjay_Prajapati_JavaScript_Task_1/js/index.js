8305ce7e3e028732b3659702c9ec5aa12de696a9
sanjayprajapati812 <svprajapati812@gmail.com>
2023-02-07 17:38:01 +0530

    sdfsd

--- Batch1_Sanjay_Prajapati_JavaScript_Task_1/js/index.js
+++ Batch1_Sanjay_Prajapati_JavaScript_Task_1/js/index.js
@@ -1,32 +1,33 @@
 const childDivElement = document.getElementById("childDivElement").innerHTML;
-let plusBtn = document.getElementById("plusBtn");
 
 // console.log(childDivElement);
 let count = 1;
 
-document.getElementById("minusBtn").disabled = true;
+enaDisBtn();
 
-function addElement() {
-  if (count < 10) {
-    document.getElementById("divElement").innerHTML += childDivElement;
-    document.getElementById("minusBtn").disabled = false;
-    count++;
-    console.log(count)
-  } else {
-    document.getElementById("limitShow").innerHTML = "limit over";
-    plusBtn.disabled = true;
+function enaDisBtn() {
+  if (count <= 1) {
+    document.getElementById("minusBtn").disabled = true;
+  }
+  if (count >= 10) {
+    document.getElementById("plusBtn").disabled = true;
+    document.getElementById("limitShow").innerHTML = "";
   }
 }
 
+function addElement() {
+  document.getElementById("divElement").innerHTML += childDivElement;
+  document.getElementById("minusBtn").disabled = false;
+  count++;
+  // console.log(count)
+  enaDisBtn();
+}
+
 function removeElement(currEle) {
-  document.getElementById("limitShow").innerHTML = "";
-  if (count <= 1) {
-    document.getElementById("minusBtn").disabled = true;
-  } else {
-    document.getElementById("minusBtn").disabled = false;
-    plusBtn.disabled = false;
-    currEle.parentNode.parentNode.remove();
-    count-- ;
-    console.log(count)
-  }
+  document.getElementById("minusBtn").disabled = true;
+  document.getElementById("minusBtn").disabled = false;
+  currEle.parentNode.parentNode.remove();
+  count--;
+  // console.log(count)
+  enaDisBtn()
 }
