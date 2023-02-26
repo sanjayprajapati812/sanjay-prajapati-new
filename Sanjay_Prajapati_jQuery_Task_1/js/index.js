c0e3011ee58647e1a4f08cb4ff63b2a8644e6b56
sanjayprajapati812 <svprajapati812@gmail.com>
2023-02-27 00:25:13 +0530

    njjjjjjjjjjjjjjjjjjj

--- Sanjay_Prajapati_jQuery_Task_1/js/index.js
+++ Sanjay_Prajapati_jQuery_Task_1/js/index.js
@@ -1,15 +1,19 @@
 var array = []
 var index;
+var regName = /^[a-zA-Z ]*$/;
+var regNumber = /^(\d{1,2}|100)$/;
 
 function getData() {
     array = []
     $("#addNewRowHere > tr").each(function () {
-        let obj = new Object();
-        obj.name = $(this).find('td').eq(0).children("input:first-child").val();
-        obj.subject = $(this).find('td').eq(1).children("input:first-child").val();
-        obj.mark = $(this).find('td').eq(2).children("input:first-child").val();
-        obj.mark < 33 ? obj.result = "Fail" : obj.result = "Pass";
-        array.push(obj)
+        if ($(this).attr('isaccepted') == 'yes') {
+            let obj = new Object();
+            obj.name = $(this).find('td').eq(0).children("input:first-child").val();
+            obj.subject = $(this).find('td').eq(1).children("input:first-child").val();
+            obj.mark = $(this).find('td').eq(2).children("input:first-child").val();
+            obj.mark < 33 ? obj.result = "Fail" : obj.result = "Pass";
+            array.push(obj);
+        }
     });
     return array
 }
@@ -49,38 +53,86 @@ $("#addRandom").on('click', function () {
         $(this).find('td').eq(1).children("input:first-child").val(randomArray[n].subject);
         $(this).find('td').eq(2).children("input:first-child").val(randomArray[n].marks);
     });
+    validateTable()
 })
 
 $("#subBtn").on('click', function () {
-    $("#reportTblBody").html("")
-    getData().forEach((item, index) => {
-        let a = $(`<tr><td>${index + 1}</td><td>${item.name}</td><td>${item.subject}</td><td>${item.mark}</td><td>${item.result}</td></tr>`).get(0);
-        item.mark < 33 ? $(a).attr("class", "table-danger") : $(a).attr("class", "table-success");
-        $("#reportTblBody").append(a);
-    })
+    if (!validateTable()) {
+        $("#mainErrShow").html("please enter all detail correct")
+    } else {
+        $("#mainErrShow").html("");
+        $("#container2").show()
+        $("#reportTblBody").html("");
+        getData().forEach((item, index) => {
+            let a = $(`<tr><td></td><td>${item.name}</td><td>${item.subject}</td><td>${item.mark}</td><td>${item.result}</td></tr>`).get(0);
+            item.mark < 33 ? $(a).attr("class", "table-danger") : $(a).attr("class", "table-success");
+            $("#reportTblBody").append(a);
+        })
+    }
 })
 
+function showNameError(tr, i, validate) {
+    if (!regName.test(tr.find('td').eq(i).children("input:first-child").val()) || !(tr.find('td').eq(i).children("input:first-child").val().trim())) {
+        tr.find('td').eq(i).children("input:first-child").attr("class", "form-control error")
+        tr.find('td').eq(i).children("div.invalid").html("*Please enter text only value")
+        return validate = false
+    } else {
+        tr.find('td').eq(i).children("input:first-child").attr("class", "form-control")
+        tr.find('td').eq(i).children("div.invalid").html("")
+    }
+    return validate
+}
 
+function showNumberError(tr, validate) {
+    if (!regNumber.test(parseInt(tr.find('td').eq(2).children("input:first-child").val()))) {
+        tr.find('td').eq(2).children("input:first-child").attr("class", "form-control error")
+        tr.find('td').eq(2).children("div.invalid").html("*Please enter valid marks(0-100)")
+        return validate = false;
+    } else {
+        tr.find('td').eq(2).children("input:first-child").attr("class", "form-control")
+        tr.find('td').eq(2).children("div.invalid").html("")
+    }
+    return validate
+}
 
+function validateTable() {
+    let validate = true;
+    $("#addNewRowHere > tr").each(function () {
+        validate = showNameError($(this), 0, validate);
+        $(this).find('td').eq(0).children("input:first-child").keyup(() => showNameError($(this), 0))
+        validate = showNameError($(this), 1, validate);
+        $(this).find('td').eq(1).children("input:first-child").keyup(() => showNameError($(this), 1))
+        validate = showNumberError($(this), validate);
+        $(this).find('td').eq(2).children("input:first-child").keyup(() => showNumberError($(this), 2))
+    })
+    return validate;
+}
 
+$('.timer').startTimer({
+    onComplete: function () {
+        $('#timerModal').modal({
+            backdrop: 'static',
+            keyboard: false
+        })
+        $('#timerModal').modal('show');
 
-
-
-
-
-
-$(document).ready(function () {
-    $('#myform').validate({ // initialize the plugin
-        rules: {
-            field1: {
-                required: true,
-                email: true
-            },
-            field2: {
-                required: true,
-                minlength: 5
-            }
-        }
-    });
-
+        $("#timerModal").on("click", "#okBtn", function () {
+            $('.timer').empty()
+            $('.timer').startTimer()
+        })
+    }
 });
+
+$(document).on('click','.btnPF', function () {
+    if ($(this).attr('class') == "btn btn-outline-success me-2 btnPF") {
+        $(this).attr('class', 'btn btn-success me-2 btnPF')
+        $(this).next().attr('class', 'btn btn-outline-danger btnPF')
+        $(this).closest('tr').attr('isAccepted', 'yes')
+    }
+    if ($(this).attr('class') == "btn btn-outline-danger btnPF") {
+        $(this).attr('class', 'btn btn-danger btnPF')
+        $(this).prev().attr('class', 'btn btn-outline-success me-2 btnPF')
+        $(this).closest('tr').attr('isAccepted', 'no')
+    }
+
+})
