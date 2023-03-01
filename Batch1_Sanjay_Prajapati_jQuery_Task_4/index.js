62b1b6d4322366562ad214ae9c13d296d94cb235
sanjayprajapati812 <svprajapati812@gmail.com>
2023-03-01 19:08:36 +0530

    dfghafdsgh

---
+++ Batch1_Sanjay_Prajapati_jQuery_Task_4/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "https://api.publicapis.org/entries",
+        dataType: "json",
+        success: function (data) {
+            $('#totalRes').html(`total number of rows :- ${data.count}`)
+            $('#spinner').hide()
+            let theader = "<th>No</th>"
+            let index = 1
+
+            Object.keys(data.entries[0]).forEach((key) => {
+                key != 'HTTPS' ? (theader += '<th>' + key + '</th>') : null
+            })
+
+            $('table').find('tr:first-child').html(theader)
+
+            for (let element of data.entries) {
+                let string = ''
+
+                $('table').find('tbody').append(`<tr>
+                <td>${index}</td>
+                ${Object.keys(element).forEach((key) => {
+                    key != 'HTTPS' ? (string += `<td> ${element[key].toString().includes('http') ? "<a href =" + element[key] + ">" + element[key] || "-" + "</a>" : '' + element[key] || '-' + ''}  </td>`) : null
+                })}
+                ${string}
+                </tr>`)
+                index++
+            }
+        }
+    });
+});
