800cf8e629c755cd33b93664bc078f6ec4a4922e
sanjayprajapati812 <svprajapati812@gmail.com>
2023-03-02 15:21:34 +0530

    ssssssssssssssssssssss

--- Batch_1_Sanjay_Prajapati_JS_General_Task/index.js
+++ Batch_1_Sanjay_Prajapati_JS_General_Task/index.js
@@ -2,11 +2,9 @@ function saveData() {
     let inputElementCollection = Array.from(document.body.firstElementChild.children).filter((element) => {
         return element instanceof HTMLInputElement;
     })
-
     let string = '';
     inputElementCollection.forEach((item, index) => {
         string += `<tr><td>${index + 1}</td><td>${item.value}</td></tr>`
     })
-
     document.querySelector('tbody').innerHTML = string
 }
